bfd81cc01a563a8ad41d2cb4414db1bfdacabb9f
Fatty Rojas <faty.aome.rojas14@gmail.com>
2024-06-21 13:47:03 -0600

    1.1 SaiiUtt

--- siiuttlax/siiuttlax/settings.py
+++ siiuttlax/siiuttlax/settings.py
@@ -1,6 +1,6 @@
 """
-Author: Francisco López Briones
-Github: @franlop24
+Author: Fatima Rojas Salamanca
+Github: 
 Project: siiuttlax
 File: settings.py
 Description: Configuraciones de Proyecto SIIUTTlax
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'siiuttlax.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': str(os.getenv('DB_NAME')),
+        'USER': str(os.getenv('DB_USER')),
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': str(os.getenv('DB_HOST')),
+        'PORT': str(os.getenv('DB_PORT')),
     }
 }
 
