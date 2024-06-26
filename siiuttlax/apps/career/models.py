37dd509fff5caf01da00deddf482714f51400005
Brandonmkmk <brandonxd611@gmail.com>
2024-06-26 14:57:11 -0600

    app career

--- siiuttlax/apps/career/models.py
+++ siiuttlax/apps/career/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Career(models.Model):
+    name = models.CharField(max_length=50)
+    shortname = models.CharField(max_length=10)
+    director = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+    plan = models.CharField(max_length=4)
+    status = models.BooleanField()
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+    semester = models.IntegerField()
+    total_hours = models.IntegerField()
+    hours_semesters = models.IntegerField()
+    file = models.CharField(max_length=50)
+
+
+
--- siiuttlax/siiuttlax/settings.py
+++ siiuttlax/siiuttlax/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.home.apps.HomeConfig',
+    'apps.career.apps.CareerConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
