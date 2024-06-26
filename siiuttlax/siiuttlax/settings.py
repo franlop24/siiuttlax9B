44e0d0e329b71a834cad894d4266c807f30f83fb
Rogelio <royhs23@gmail.com>
2024-06-26 14:50:36 -0600

    Creacion de aplicacion period y la carpeta periods en templates

--- siiuttlax/siiuttlax/settings.py
+++ siiuttlax/siiuttlax/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.home.apps.HomeConfig',
-
+    'period.apps.PeriodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
