28ec2cd3f193ac3e97dfb2d9d5c92de814c7395b
Fatty Rojas <faty.aome.rojas14@gmail.com>
2024-07-15 11:22:27 -0600

    App Academy

--- siiuttlax/siiuttlax/settings.py
+++ siiuttlax/siiuttlax/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'apps.home.apps.HomeConfig',
     'apps.academy.apps.AcademyConfig',
-
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
