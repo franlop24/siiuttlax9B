c4cba0602a6df14d8834e1ef21ff27897f3fd781
Rogelio <royhs23@gmail.com>
2024-07-17 14:58:27 -0600

    fix urls.py in period app

--- siiuttlax/apps/period/urls.py
+++ siiuttlax/apps/period/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'period'
 urlpatterns = [
-    path('periods/', views.home, name='home'),
+    path('', views.home, name='home'),
 ]
